fabd11a864798adaab7e17f6e772b5f1353b83a9
studsch <113303183+studsch@users.noreply.github.com>
2022-11-12 18:16:20 +0700

    replace views

--- 4/serv/four/views.py
+++ 4/serv/four/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import *
+import random
 
-# Create your views here.
+
+Groups(num_of_students=5, surname_of_the_elder='Aristotle').save()
+print(Groups.objects.get(pk=1))
+students = Students.objects.all()
+print(f"All students in DB: {students}")
+
+for i in range(10):
+    gender = random.choice(("m", "f"))
+    Students(
+        surname=f"student{i}",
+        gender=gender,
+        group_code=Groups.objects.get(pk=1)
+    ).save()
+
+for stud in students:
+    print(stud)
+
+print("All female students:")
+for female in students.filter(gender="f"):
+    print(female)
